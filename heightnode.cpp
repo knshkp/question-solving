2d452f4480fff5078733c4f14e1fff7b965b85f8
Kanishk Pareek <knshkpareek@gmail.com>
2022-12-07 16:10:29 +0530

    dsjn

---
+++ heightnode.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+struct Node{
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data=val;
+        left=NULL;
+        right=NULL;
+    }
+};
+void level(Node* root){
+    if(root==NULL){
+        return;
+    }
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    while(!q.empty()){
+        Node* node=q.front();
+        q.pop();
+        if(node!=NULL){
+            cout<<node->data<<" ";
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){
+                q.push(node->right);
+            }
+        }
+        else if(!q.empty()){
+            q.push(NULL);
+        }
+    }
+}
+int height(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int lheight=height(root->left);
+    int rheight=height(root->right);
+    return max(lheight,rheight)+1;
+}
+int diameter(Node* root){
+    if(root==NULL){
+        return 0;
+    }
+    int currdiameter=height(root->left)+height(root->right)+1;
+    int ldiameter=diameter(root->left);
+    int rdiameter=diameter(root->right);
+    return  max(currdiameter,max(ldiameter,rdiameter))+1;
+}
+int sumk(Node* root,int k){
+    if(root==NULL){
+        return -1;
+    }
+    queue<Node*> q;
+    q.push(root);
+    q.push(NULL);
+    int level=0;
+    int sum=0;
+    while(!q.empty()){
+        Node* node=q.front();
+        q.pop();
+        if(node!=NULL){
+            if(level==k){
+                sum+=node->data;
+            };
+            if(node->left){
+                q.push(node->left);
+            }
+            if(node->right){
+                q.push(node->right);
+            }
+            
+        }
+        else if(!q.empty()){
+            q.push(NULL);
+            level+=1;
+        }
+    }
+    return sum;
+}
+int main(){
+    Node* root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    root->right->right=new Node(7);
+    level(root);
+    cout<<sumk(root,2)<<" "<<height(root)<<endl<<diameter(root);
+    return 0;
+}
